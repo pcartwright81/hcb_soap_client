ee57fd7c782a3611e67957d5d68cace063b9a06b
Patrick Cartwright <pcartwright1981@gmail.com>
2026-03-06 16:10:46 -0600

    bugfix: fix empty start offset and update `pyproject.toml` accordingly.

--- hcb_soap_client/stop_response.py
+++ hcb_soap_client/stop_response.py
@@ -18,6 +18,10 @@ def _parse_float(value: str) -> float:
     """Parse float from string, defaulting to 0 for empty."""
     return 0.0 if value == "" else float(value)
 
+def _parse_int(value: str) -> int:
+    """Parse int from string, defaulting to 0 for empty."""
+    return 0 if value == "" else int(value)
+
 
 def _parse_datetime(value: str) -> datetime:
     """Parse datetime string."""
@@ -25,6 +29,7 @@ def _parse_datetime(value: str) -> datetime:
 
 
 FloatStr = Annotated[float, BeforeValidator(_parse_float)]
+IntStr = Annotated[int, BeforeValidator(_parse_int)]
 DateTimeStr = Annotated[datetime, BeforeValidator(_parse_datetime)]
 
 
@@ -44,7 +49,7 @@ class StudentStop(BaseModel):
     vehicle_id: str
     esn: str
     tier_start_time: TimeStr
-    bus_visibility_start_offset: int
+    bus_visibility_start_offset: IntStr
 
     @classmethod
     def from_element(cls, elem: etree._Element) -> "StudentStop":
@@ -63,7 +68,7 @@ class StudentStop(BaseModel):
             vehicle_id=elem.get("VehicleId", ""),
             esn=elem.get("Esn", ""),
             tier_start_time=elem.get("TierStartTime", "00:00:00"),
-            bus_visibility_start_offset=int(elem.get("BusVisibilityStartOffset", "0")),
+            bus_visibility_start_offset=elem.get("BusVisibilityStartOffset", "0"),
         )
 
 
